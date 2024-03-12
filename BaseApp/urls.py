123a280e733e9c0b743870521b0e599d9710fb89
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2024-03-12 13:52:42 +0000

    New homepage for better CSS integration

--- BaseApp/urls.py
+++ BaseApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name="home"),
+    path('', views.home, name="home"),
 
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
